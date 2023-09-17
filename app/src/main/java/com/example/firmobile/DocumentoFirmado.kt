1f607fce3bbd4ca33dc4173df27ddca7643cd44f
Walter Lopez <wglopez100@gmail.com>
2023-09-17 11:51:16 -0300

    17-09-2023 11.51 hs

---
+++ app/src/main/java/com/example/firmobile/DocumentoFirmado.kt
@@ -0,0 +1,11 @@
+package com.example.firmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DocumentoFirmado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_documento_firmado)
+    }
+}
---
+++ app/src/main/java/com/example/firmobile/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.firmobile
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+//        var documentos: ArrayList<Documento> = ArrayList();
+//
+//        documentos.add(Documento(1, "pruebaDoc.doc", "directorioPrueba/prueba.doc", "Doc", R.drawable.doc_icon));
+//        documentos.add(Documento(2, "pruebaDocx.docx", "directorioPrueba/prueba.docx", "Docx", R.drawable.docx_icon))
+//        documentos.add(Documento(3, "pruebaPDF.pdf", "directorioPrueba/prueba.pdf", "PDF", R.drawable.pdf_icon));
+//        documentos.add(Documento(4, "pruebaODT.odt", "directorioPrueba/prueba.odt", "ODT", R.drawable.odt_icon))
+
+
+//        val adaptador = AdapterDocumento(this, documentos)
+//
+//
+//        val miLista = findViewById<View>(R.id.documentos) as ListView
+//        miLista.adapter = adaptador
+
+    }
+
+}
